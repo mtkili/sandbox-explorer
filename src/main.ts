4bc88452392d0974e75a5af0268e44be4f61d232
mtkili <66521617+mtkili@users.noreply.github.com>
2020-12-12 12:38:14 +0100

    changes

---
+++ src/main.ts
@@ -0,0 +1,148 @@
+class DrawingApp {
+    private canvas: HTMLCanvasElement;
+    private myContext: CanvasRenderingContext2D;
+    private paint: boolean;
+
+    private clickX: number[] = [];
+    private clickY: number[] = [];
+    private clickDrag: boolean[] = [];
+
+    constructor() {
+        let canvas = document.getElementById(
+            "canvas",
+        ) as HTMLCanvasElement;
+        let context = canvas.getContext("2d");
+        if (context) {
+            this.myContext = context;
+            this.myContext.lineCap = "round";
+            this.myContext.lineJoin = "round";
+            this.myContext.strokeStyle = "black";
+            this.myContext.lineWidth = 1;
+        } else {
+            console.error("No canvas found!");
+        }
+
+        this.canvas = canvas;
+        this.paint = false;
+        this.redraw();
+        this.createUserEvents();
+    }
+
+    private createUserEvents() {
+        let canvas = this.canvas;
+
+        canvas.addEventListener(
+            "mousedown",
+            this.pressEventHandler,
+        );
+        canvas.addEventListener(
+            "mousemove",
+            this.dragEventHandler,
+        );
+        canvas.addEventListener(
+            "mouseup",
+            this.releaseEventHandler,
+        );
+        canvas.addEventListener(
+            "mouseout",
+            this.cancelEventHandler,
+        );
+
+        canvas.addEventListener(
+            "touchstart",
+            this.pressEventHandler,
+        );
+        canvas.addEventListener(
+            "touchmove",
+            this.dragEventHandler,
+        );
+        canvas.addEventListener(
+            "touchend",
+            this.releaseEventHandler,
+        );
+        canvas.addEventListener(
+            "touchcancel",
+            this.cancelEventHandler,
+        );
+    }
+
+    private redraw() {
+        let clickX = this.clickX;
+        let context = this.myContext;
+        let clickDrag = this.clickDrag;
+        let clickY = this.clickY;
+        for (let i = 0; i < clickX.length; ++i) {
+            context.beginPath();
+            if (clickDrag[i] && i) {
+                context.moveTo(
+                    clickX[i - 1],
+                    clickY[i - 1],
+                );
+            } else {
+                context.moveTo(clickX[i] - 1, clickY[i]);
+            }
+
+            context.lineTo(clickX[i], clickY[i]);
+            context.stroke();
+        }
+        context.closePath();
+    }
+
+    private addClick(
+        x: number,
+        y: number,
+        dragging: boolean,
+    ) {
+        this.clickX.push(x);
+        this.clickY.push(y);
+        this.clickDrag.push(dragging);
+    }
+
+    private releaseEventHandler = () => {
+        this.paint = false;
+        this.redraw();
+    };
+
+    private cancelEventHandler = () => {
+        this.paint = false;
+    };
+
+    private pressEventHandler = (
+        e: MouseEvent | TouchEvent,
+    ) => {
+        let mouseX = (e as TouchEvent).changedTouches
+            ? (e as TouchEvent).changedTouches[0].pageX
+            : (e as MouseEvent).pageX;
+        let mouseY = (e as TouchEvent).changedTouches
+            ? (e as TouchEvent).changedTouches[0].pageY
+            : (e as MouseEvent).pageY;
+        mouseX -= this.canvas.offsetLeft;
+        mouseY -= this.canvas.offsetTop;
+
+        this.paint = true;
+        this.addClick(mouseX, mouseY, false);
+        this.redraw();
+    };
+
+    private dragEventHandler = (
+        e: MouseEvent | TouchEvent,
+    ) => {
+        let mouseX = (e as TouchEvent).changedTouches
+            ? (e as TouchEvent).changedTouches[0].pageX
+            : (e as MouseEvent).pageX;
+        let mouseY = (e as TouchEvent).changedTouches
+            ? (e as TouchEvent).changedTouches[0].pageY
+            : (e as MouseEvent).pageY;
+        mouseX -= this.canvas.offsetLeft;
+        mouseY -= this.canvas.offsetTop;
+
+        if (this.paint) {
+            this.addClick(mouseX, mouseY, true);
+            this.redraw();
+        }
+
+        e.preventDefault();
+    };
+}
+
+new DrawingApp();
